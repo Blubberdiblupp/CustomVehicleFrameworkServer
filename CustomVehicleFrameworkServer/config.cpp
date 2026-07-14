13ea88f455f38e851436daa44d430d3c00fee607
Blubberdiblupp <baldensperger@gmx.ch>
2026-07-15 01:41:03 +0200

    Add files via upload

---
+++ CustomVehicleFrameworkServer/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+    class CustomVehicleFramework_GeneratedOverrides
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"CustomVehicleFramework"};
+    };
+};
+
+class CfgCustomVehicleFramework
+{
+    class GeneratedOverrideProbe
+    {
+        cvfProtocol = 0;
+        cvfPayloadHash = 0;
+        cvfPayloadChars = 0;
+        cvfServerId = "";
+    };
+};
